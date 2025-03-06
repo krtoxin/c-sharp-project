1e370e95ecbbad372564c7e055744847a8b4436e
krtoxin <kira.kutas@oa.edu.ua>
2025-03-06 02:00:18 +0200

    some changes

--- src/StudyBuddy.WebApp/Program.cs
+++ src/StudyBuddy.WebApp/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using StudyBuddy.Data;
+using StudyBuddy.WebApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
