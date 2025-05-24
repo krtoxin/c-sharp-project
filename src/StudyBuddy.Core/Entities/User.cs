e9e4da3d5dd4e7010c4486f65a1af910ca3327ce
krtoxin <kira.kutas@oa.edu.ua>
2025-05-24 17:20:41 +0300

    Update profile

--- src/StudyBuddy.Core/Entities/User.cs
+++ src/StudyBuddy.Core/Entities/User.cs
@@ -7,15 +7,20 @@
         public string Email { get; set; } = null!;
         public string PasswordHash { get; set; } = null!;
         public string FullName { get; set; } = null!;
+        public string? DisplayName { get; set; } 
         public string? ProfileImage { get; set; }
+        public bool IsPremiumUser { get; set; } = false; 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public bool IsOnline { get; set; }
         public DateTime LastActive { get; set; }
+
         public int RoleId { get; set; }
         public Role Role { get; set; } = null!;
+
         public List<UserSubject> UserSubjects { get; set; } = new();
         public List<UserProgress> Progresses { get; set; } = new();
         public List<ChatRoomMember> ChatRooms { get; set; } = new();
         public List<ChatMessage> SentMessages { get; set; } = new();
     }
+
 }
---
+++ src/StudyBuddy.Core/Migrations/20250524135613_AddDisplayNameAndPremiumToUser.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudyBuddy.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDisplayNameAndPremiumToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPremiumUser",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsPremiumUser",
+                table: "Users");
+        }
+    }
+}
